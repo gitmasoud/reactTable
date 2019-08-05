f390f02dd040221618cdfb18eaa43848fbae834a
gitmasoud <shuaibam1@gmail.com>
2019-08-05 09:12:09 +0100

    Create Table.js

---
+++ Table.js
@@ -0,0 +1,12 @@
+// export default App;
+import React, { Component } from 'react';
+import DatatablePage from './datatables';
+export default class Table extends Component {
+  render() {
+    return (
+      <div className="App">
+        <DatatablePage />
+      </div>
+    );
+  }
+}
